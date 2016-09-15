2509f160c4c74d5a8fe099b1b317d9ce52cfdfc0
Diego Giacomelli <giacomelli@gmail.com>
2016-09-14 22:32:42 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Buildron-ConsoleMod
+![](docs/images/Buildron-ConsoleMod.png)
+
+Mod that adds a console window to [Buildron](https://github.com/skahal/buildron) and let you monitor his events.
+
+### FAQ
+Having troubles? 
+
+Ask on Twitter [@ogiacomelli](http://twitter.com/ogiacomelli).
+ 
+ 
+### How to improve it?
+
+Create a fork of [jumpstart](https://github.com/giacomelli/Buildron-ConsoleMod/fork). 
+
+Did you change it? [Submit a pull request](https://github.com/giacomelli/Buildron-ConsoleMod/pull/new/master).
+
+
+### License
+Licensed under the The MIT License (MIT).
+In others words, you can use this library for developement any kind of software: open source, commercial, proprietary and alien.
---
+++ src/Code/ConsoleMod/ModController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Show to the user a window where the Buildron events will be logged. 
+/// It's a very simple Unity3d MonoBehaviour that use some GUILayout stuffs to build its UI.
+/// </summary>
+public class ModController : MonoBehaviour
+{
+	#region Fields
+	private string m_title;
+	private Rect m_windowRect = new Rect(10, 10, 400, 300);
+	private List<string> m_msgs = new List<string>();
+	#endregion
+
+	#region Constructors
+	public ModController()
+	{
+		m_title = "Console mod (v.{0})".With(GetType().Assembly.GetName().Version);
+	}
+    #endregion
+
+    #region Methods    
+    /// <summary>
+    /// Adds the message to the console window.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="args">The arguments.</param>
+    public void AddMessage(string message, params object[] args)
+	{
+		var formattedMessage = message.With(args);
+		m_msgs.Insert(0, "[{0:HH:mm:ss}] {1}".With(DateTime.Now, formattedMessage));
+
+		if (m_msgs.Count > 10)
+		{
+			m_msgs.RemoveAt(10);
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Window(1, m_windowRect, HandleWindowFunction, m_title, GUILayout.MinWidth(100), GUILayout.MinHeight(100));
+	}
+
+	void HandleWindowFunction(int id)
+	{
+		GUILayout.BeginVertical();
+
+		foreach (var msg in m_msgs)
+		{
+			GUILayout.Label(msg);
+		}
+
+		GUILayout.EndVertical();
+	}
+	#endregion
+}
